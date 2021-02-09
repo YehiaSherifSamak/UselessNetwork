5c934a3333d919aba6f036d5ff7a991d83b66493
Yehia Samak <mostafa.mohsen@softxpert.com>
2021-02-09 13:15:31 +0200

    Initial Commit

---
+++ UselessNetwork/UselessNetwork.h
@@ -0,0 +1,18 @@
+//
+//  UselessNetwork.h
+//  UselessNetwork
+//
+//  Created by Soft Xpert on 2/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UselessNetwork.
+FOUNDATION_EXPORT double UselessNetworkVersionNumber;
+
+//! Project version string for UselessNetwork.
+FOUNDATION_EXPORT const unsigned char UselessNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UselessNetwork/PublicHeader.h>
+
+
